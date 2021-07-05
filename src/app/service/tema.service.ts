5078beba869ad75002c3e4d5bf3398bf56791868
ismarps <ismarprobst@gmail.com>
2021-07-05 17:21:27 -0300

    feat metodos de put e delete do tema

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -17,12 +17,29 @@ export class TemaService {
     headers: new HttpHeaders().set('Authorization',environment.token)
   }
 
+  refreshToken(){
+    this.token = {
+      headers: new HttpHeaders().set('Authorization',environment.token)
+    }
+  }
+
   getAllTema():Observable<Tema[]>{
     return this.http.get<Tema[]>('https://blogpessoal-backend-ismarps.herokuapp.com/tema',this.token)
+  }
+
+  getByIdTema(id: number): Observable<Tema>{
+    return this.http.get<Tema>(`https://blogpessoal-backend-ismarps.herokuapp.com/tema/${id}`, this.token)
   }
 
   postTema(tema: Tema):Observable<Tema>{
     return this.http.post<Tema>('https://blogpessoal-backend-ismarps.herokuapp.com/tema',tema,this.token)
   }
   
+  putTema(tema: Tema): Observable<Tema>{
+    return this.http.put<Tema>('https://blogpessoal-backend-ismarps.herokuapp.com/tema', tema, this.token)
+  }
+
+  deleteTema(id: number){
+    return this.http.delete(`https://blogpessoal-backend-ismarps.herokuapp.com/tema/${id}`,this.token)
+  }
 }
